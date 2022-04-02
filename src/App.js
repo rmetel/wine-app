fb0bd6958deb394feabce77b75eb3e042c958ac4
ralph.metel@gmx.de <pOison_2(>
2022-04-02 16:08:31 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import {Button} from 'reactstrap';
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav/>
+      <Button color="primary" className="btn-simple btn-round btn btn-primary">Add to cart</Button>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+// reactstrap components
+import {
+  Button,
+  NavItem,
+  NavLink,
+  Nav,
+  Container,
+  Row,
+  Col,
+  UncontrolledTooltip,
+} from "reactstrap";
+
+class Footer extends React.Component {
+  render(){
+    return (
+        <div>
+          footer
+        </div>
+      );
+  }
+}
+
+export default Footer;
