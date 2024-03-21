fb36d5302b4f8268d937a9c2317ca7ef478608e5
ralph.metel@gmx.de <pOison_2(>
2024-03-21 21:00:09 +0100

    transform to tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
-export const NavBar = () => {
+export const NavBar: React.FC = () => {
   return (
     <Navbar
       collapseOnSelect
